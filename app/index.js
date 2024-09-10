71df768881cd581df9db7707ea078b17c1d315cf
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-10 06:05:40 -0300

    update index.js
    O index será o arquivo playground

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// arrays, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value);
