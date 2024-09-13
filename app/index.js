99ea7c885a8bff55fcdb5ed27df72ab9694720a0
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-12 22:29:16 -0300

    adiciona metas abertas

--- app/index.js
+++ app/index.js
@@ -59,10 +59,27 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas " + realizadas.length,
         choices: [...realizadas]
     })
     
+}
+
+// agua [] - caminhar [] - cantar [x]
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :)");
+        return
+    }
+
+    await select({
+        message: "Metas Abertas " + abertas.length,
+        choices: [...abertas]
+    })
 }
 
 const start = async () => {
@@ -85,6 +102,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -102,6 +123,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima");
