c647d05b93dfc709c38388d3a67b61e1fd27e6ec
gaabrieloliver <contato.gabrieloliveira0@gmail.com>
2024-09-12 22:02:33 -0300

    apresenta metas realizadas

--- app/index.js
+++ app/index.js
@@ -48,6 +48,23 @@ const listarMetas = async () => {
     
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas: :(');
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+    
+}
+
 const start = async () => {
 
     while (true) {
@@ -64,6 +81,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -78,6 +99,9 @@ const start = async () => {
             case "listar":
                 await listarMetas();
                 console.log("Vamos listar");
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima");
